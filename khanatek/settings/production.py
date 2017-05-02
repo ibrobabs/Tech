e158ee85545e78fb371e38bda976fdb85a687d38
ibrobabs <babskolawole@gmail.com>
2017-05-02 14:23:18 -0600

    elastic

--- khanatek/settings/production.py
+++ khanatek/settings/production.py
@@ -43,16 +43,16 @@ WAGTAILSEARCH_BACKENDS = {
 }
 
 
-CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': '127.0.0.1:6379',
-        'KEY_PREFIX': 'khanatek',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
-        }
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'redis_cache.cache.RedisCache',
+#         'LOCATION': '127.0.0.1:6379',
+#         'KEY_PREFIX': 'khanatek',
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+#         }
+#     }
+# }
 
 try:
     from .local import *
