afcc56e8328a95db4d3143c2a2cbe6b8e41d0e39
ibrobabs <babskolawole@gmail.com>
2017-05-08 11:40:03 -0600

    updated css

--- khanatek/settings/dev.py
+++ khanatek/settings/dev.py
@@ -10,7 +10,7 @@ import os
 DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = True
 
-SECRET_KEY='riv_tsm@)$i+&$plnqm1i5h4m4yhr2#z%#465o4i&_*d-9nlws'
+# SECRET_KEY='SECRET'
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
